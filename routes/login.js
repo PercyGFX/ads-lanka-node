5576011f3c0292a2c621def14e7e019bf4f905ed
PercyGFX <isurangabtk@gmail.com>
2023-04-20 13:33:46 +0530

    authMiddleware

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const authentication = require('../authentication')
 const loginController = require('../controllers/loginController');
 
-router.get('/login', (req, res) => {
+router.get('/login',  (req, res) => {
     if (req.session.phone) {
       res.redirect('/');
     } else {
@@ -23,6 +24,12 @@ router.get('/login', (req, res) => {
 
   })
   
-  router.post('/login', loginController.login);
+  router.post('/login',  loginController.login);
+
+  router.get('/usercp', authentication, (req,res)=>{
+
+    res.send('this is user panel')
+
+  })
   
   module.exports = router;
